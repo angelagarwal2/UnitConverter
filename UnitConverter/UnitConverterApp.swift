51109bf248e125af212e5db4cbd08ed8992e96ea
angelagarwal2 <angel.agarwal28feb@gmail.com>
2025-04-01 18:07:58 +0530

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Angel on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
